4dbf9b5a149cae45ed72a486b232426f70dfc97c
Jules Martin <julesmartin76000@gmail.com>
2017-07-01 11:11:55 +0200

    Problème avec rake fixé

--- PewNB/config/application.rb
+++ PewNB/config/application.rb
@@ -11,7 +11,7 @@ module PewNB
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = :"fr"
-    I18n.locale = :"fr"
+    #config.i18n.default_locale = :"fr"
+    #I18n.locale = :"fr"
   end
 end
